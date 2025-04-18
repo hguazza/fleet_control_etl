79f994c680c972fb2dca09212aa998c32314016b
hguazza <144302319+hguazza@users.noreply.github.com>
2025-04-18 18:18:31 -0300

    func extract_from_google_sheets created

--- src/auto_fleet_control/etl.py
+++ src/auto_fleet_control/etl.py
@@ -7,7 +7,9 @@ from datetime import datetime
 
 log_file = "log_file.txt"
 target_file = "transformed_data.csv"
-xml_path = "C:\\Users\\Henrique\\Dev\\Python\\auto_fleet_control\\notas_xml\\test.xml"
+nfe_paths = "C:\\Users\\Henrique\\Dev\\Python\\auto_fleet_control\\notas_xml\\*.xml"
+sheet_url = "https://docs.google.com/spreadsheets/d/12qIUsfXlwG_qnb7efwA2aAM2A3_oI3E0BDQA0l0Xfxo/edit?gid=903440851#gid=903440851"
+
 
 def setup_logging():
     """Configure logging for the application."""
@@ -16,9 +18,26 @@ def setup_logging():
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     )
 
-def extract_from_xml(xml_path):
+def extract_from_google_sheets(sheet_url):
+    """Read a Google Sheet and convert it to a DataFrame."""
+
+    # The part of the URL that contains the sheet ID
+    sheet_id = sheet_url.split("/d/")[1].split("/edit")[0]
+    # Construct the CSV export URL
+    csv_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid=903440851"
+
+    try:
+        df = pd.read_csv(csv_url)
+        print("DataFrame created successfully:")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        print("Please ensure the Google Sheet is publicly accessible.")
+    return df
+
+
+def extract_from_xml(xml_file):
     dataframe = pd.DataFrame(columns=["num NF", "valor NF", "descricao produto/servico"])
-    tree = ET.parse(xml_path)
+    tree = ET.parse(xml_file)
     root = tree.getroot()
     for NFe in root:
         nNF = NFe.find("nNF").text
@@ -34,13 +53,17 @@ def main():
     """Main entry point for the application."""
     setup_logging()
     logging.info("Application started.")
-    logging.info("Extracting data from XML files...")
-    extract_from_xml(xml_path)
-    logging.info("XML data extraction complete.")
-    logging.info("Loading data to CSV file...")
-    load_data(target_file, extract_from_xml(xml_path))
-    logging.info("Data loaded to CSV file.")
-    logging.info("Application finished.")
+    logging.info("Reading data from Google Sheet...")
+    df = extract_from_google_sheets(sheet_url)
+    logging.info("Data read from Google Sheet.")
+    # logging.info("Extracting data from XML files...")
+    # extract_from_xml(xml_path)
+    # logging.info("XML data extraction complete.")
+    # logging.info("Loading data to CSV file...")
+    # load_data(target_file, extract_from_xml(xml_path))
+    # logging.info("Data loaded to CSV file.")
+    # logging.info("Application finished.")
+    print(df)
 
 if __name__ == "__main__":
     main()
