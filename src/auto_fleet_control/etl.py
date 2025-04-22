4683c1016ea816153820389493d06cb3cb95f1ca
hguazza <144302319+hguazza@users.noreply.github.com>
2025-04-22 19:08:13 -0300

    extracting ok, loading to csv ok, not loading to sheets

--- src/auto_fleet_control/etl.py
+++ src/auto_fleet_control/etl.py
@@ -1,6 +1,7 @@
 from extract_from_sheets import extract_from_google_sheets
 from cfe_improved_v2 import extract_all_cfe
 from nfe_improved_v2 import extract_all_nfe
+from load import load_data_to_google_sheets
 import logging
 import pandas as pd
 import xml.etree.ElementTree as ET
@@ -9,7 +10,7 @@ from pathlib import Path
 
 log_file = "log_file.txt"
 target_file = "final.csv"
-sheet_url = "https://docs.google.com/spreadsheets/d/12qIUsfXlwG_qnb7efwA2aAM2A3_oI3E0BDQA0l0Xfxo/edit?gid=903440851#gid=903440851"
+sheet_url = "https://docs.google.com/spreadsheets/d/1HtXXEe58zNsG4I3k7qZLBrHJUlmruJEj9jYNffV6w0c/edit?gid=0#gid=0"
 cfe_fodler = Path("C:/Users/Henrique/Dev/Python/auto_fleet_control/cupons_xml")
 nfe_folder = Path("C:/Users/Henrique/Dev/Python/auto_fleet_control/notas_xml")
 
@@ -27,8 +28,8 @@ def extract(sheet_url, cfe_folder, nfe_folder):
     df_nfe = extract_all_nfe(nfe_folder)
     return pd.concat([df, df_cfe, df_nfe], ignore_index=True)
 
-def load_data(target_file, transformed_data):
-    transformed_data.to_csv(target_file)
+def load_data_to_csv(target_file, df):
+    df.to_csv(target_file, index=False)
 
 def main():
     """Main entry point for the application."""
@@ -37,14 +38,13 @@ def main():
     logging.info("Extracting data...")
     df = extract(sheet_url, cfe_fodler, nfe_folder)
     logging.info("Data extracted.")
-    # logging.info("Extracting data from XML files...")
-    # extract_from_xml(xml_path)
-    # logging.info("XML data extraction complete.")
-    # logging.info("Loading data to CSV file...")
-    # load_data(target_file, extract_from_xml(xml_path))
-    # logging.info("Data loaded to CSV file.")
-    # logging.info("Application finished.")
-    print(df)
+    logging.info("Loading data to CSV file...")
+    load_data_to_csv(target_file, df)
+    logging.info("Data loaded to CSV file.")
+    logging.info("Loading data to Google Sheets file...")
+    load_data_to_google_sheets(sheet_url, df)
+    logging.info("Application finished.")
+
 
 if __name__ == "__main__":
     main()
--- src/auto_fleet_control/extract_from_sheets.py
+++ src/auto_fleet_control/extract_from_sheets.py
@@ -5,9 +5,10 @@ def extract_from_google_sheets(sheet_url):
 
     # The part of the URL that contains the sheet ID
     sheet_id = sheet_url.split("/d/")[1].split("/edit")[0]
+    gid = sheet_url.split("gid=")[1] if "gid=" in sheet_url else "0"
     
     # Construct the CSV export URL
-    csv_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid=903440851"
+    csv_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid}"
 
     try:
         df = pd.read_csv(csv_url)
